25c61ec610c76fc8de5340d0da9d39e0cc0a2057
zhangxiaoguang <zhangx7635093@163.com>
2018-02-09 15:16:24 +0800

    Upgrade pod to adjust each target and add a README.

---
+++ README.md
@@ -0,0 +1,4 @@
+
+#Code_Swift
+
+ It's for learning by myself, if it has mistake, please tell me.
---
+++ code1/code2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  code2
+//
+//  Created by mac on 2018/2/9.
+//  Copyright © 2018年 -. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var lab : UILabel?
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
